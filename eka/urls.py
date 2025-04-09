ad66918a0ad7ccaa3c50498ec0a90ffb391f4587
Vignesh <78254333+Vignesh-AK@users.noreply.github.com>
2025-04-10 00:18:24 +0530

    Fix URL patterns by adding trailing slashes for consistency

--- eka/urls.py
+++ eka/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('user', include('users.urls')),
-    path('payment', include('payment.urls')),
+    path('payment/', include('payment.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
